7afc9489510992c3ccc58cffc0876fd8f1e7cb8b
Sunisa Thisansang <sunisa.pampam@gmail.com>
2022-10-21 16:24:33 +0700

    text

---
+++ admin/index.php
@@ -0,0 +1,68 @@
+<?php
+require_once('../connections/mysqli.php');
+
+if ($_SESSION == NULL) {
+  header("location:../login.php");
+  exit();
+}elseif ($_SESSION["user_level"] != "admin") {
+  header("location:../index.php");
+  exit();
+}
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/dashboard.css">
+    <title>ระบบหลังบ้าน</title>
+  </head>
+  <body>
+    <?php include 'include/header.php'; ?>
+    <div class="container-fluid">
+      <div class="row">
+        <?php include 'include/sidebarMenu.php'; ?>
+        <h2><b>บริการเสริม</b></h2>
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary addser" style="background-color: #EF774B; border:#EF774B;" data-bs-toggle="modal" data-bs-target="#addser">
+        เพิ่มบริการเสริม
+    </button>
+
+    <table id="myTable" class="display" style="width: 100%;">
+        <thead>
+            <tr>
+                <th>ลำดับ</th>
+                <th>บริการเสริม</th>
+                <th>ราคา</th>
+                <th>จัดการ</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?Php
+            require_once('../fuction/connectDB.php');
+            $result = $dbconn->prepare("SELECT * FROM onservice");
+            $result->execute();
+            for($i=0; $row = $result->fetch(); $i++){
+        ?>
+            <tr>
+                <td><?php echo $row['SV_ID']; ?></td>
+                <td><?php echo $row['SV_Name']; ?></td>
+                <td><?php echo $row['SV_Price']; ?></td>
+                <td>
+                    <div class="edit-delete">
+                        <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editser"><i class='bx bx-edit-alt'></i>แก้ไข</button>
+                        <a type="button"  href="../fuction/deleteservice.php=<?php echo $row['SV_ID'] ?>" onclick="return confirm('คุณต้องการลบบริการเสริมนี้ใช่หรือไม่')" class="btn btn-danger"  style="background-color: #DB1414 ; border-radius :10px; height: 5vh;"><span class="bx bx-trash"></i>ลบ</a>
+                    </div>
+                </td>
+            </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+      </div>
+    </div>
+    <script src="../assets/js/bootstrap.bundle.min.js"></script>
+    <?php mysqli_close($Connection); ?>
+  </body>
+</html>
---
+++ includes/importlink.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <!-- icon -->
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Mitr:wght@300&display=swap');
+  </style>
+  <!-- jquery table -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <!-- JavaScript ของ DataTables -->
+  <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+</head>
+
+</html>
